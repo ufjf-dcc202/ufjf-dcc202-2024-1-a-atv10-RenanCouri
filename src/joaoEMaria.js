e49d081d92dd988d7b67a743a5bf6a420955b7cf
Renan Couri <renancouri123@gmail.com>
2024-09-10 23:04:42 -0300

    Inicializa variáveis joao e maria,cria funções get e exporta funções.

--- src/joaoEMaria.js
+++ src/joaoEMaria.js
@@ -1,1 +1,12 @@
-console.log('modulo joaoEMaria');
+console.log('módulo joaoEMaria');
+
+let joao=1;
+let maria=2;
+
+function getJoao(){
+    return joao;
+}
+function getMaria(){
+    return maria;
+}
+export{ getJoao, getMaria };
